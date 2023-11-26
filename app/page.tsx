d3852c412bf8a9837c8910262cb6a0b43cebd767
jumpalottahigh <georgiyanev.gy@gmail.com>
2023-11-26 21:19:55 +0200

    Start prototyping

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,192 @@
-import Image from 'next/image'
+'use client'
+
+import React from 'react'
+
+type TodoItem = {
+  title: string
+  priority: 1 | 2 | 3 | 4
+  dateAdded: string
+  completed: boolean
+}
+
+// Priorities:
+// 1 - do today
+// 2 - do this week
+// 3 - do at some point in the future
+// 4 - idea bank
+
+// Future fields to add
+// dateUpdated: "2023-11-26T18:32:59.330Z", // TODO: future addition
+const data = [
+  {
+    title: 'Clean out the refrigerator',
+    priority: 2,
+    dateAdded: '2023-11-26T19:45:12.120Z',
+    completed: false,
+  },
+  {
+    title: 'Organize bookshelf',
+    priority: 4,
+    dateAdded: '2023-11-26T20:15:28.512Z',
+    completed: false,
+  },
+  {
+    title: 'Water the plants',
+    priority: 1,
+    dateAdded: '2023-11-27T08:00:00.000Z',
+    completed: false,
+  },
+  {
+    title: 'Create a shopping list',
+    priority: 2,
+    dateAdded: '2023-11-27T10:30:45.789Z',
+    completed: false,
+  },
+  {
+    title: 'Reply to emails',
+    priority: 3,
+    dateAdded: '2023-11-27T14:20:33.225Z',
+    completed: false,
+  },
+  {
+    title: 'Exercise for 30 minutes',
+    priority: 1,
+    dateAdded: '2023-11-28T07:45:00.000Z',
+    completed: false,
+  },
+  {
+    title: 'Write in journal',
+    priority: 2,
+    dateAdded: '2023-11-28T09:15:18.678Z',
+    completed: false,
+  },
+  {
+    title: 'Prepare dinner',
+    priority: 3,
+    dateAdded: '2023-11-28T18:00:00.000Z',
+    completed: false,
+  },
+  {
+    title: 'Review project proposal',
+    priority: 1,
+    dateAdded: '2023-11-29T13:40:55.123Z',
+    completed: false,
+  },
+  {
+    title: 'Set goals for the week',
+    priority: 2,
+    dateAdded: '2023-11-29T16:55:29.876Z',
+    completed: false,
+  },
+] as TodoItem[]
 
 export default function Home() {
+  const [currentTodo, setCurrentTodo] = React.useState('')
+  const [prio1Todos, setPrio1Todos] = React.useState<TodoItem[]>([])
+  const [prio2Todos, setPrio2Todos] = React.useState<TodoItem[]>([])
+  const [prio3Todos, setPrio3Todos] = React.useState<TodoItem[]>([])
+  const [prio4Todos, setPrio4Todos] = React.useState<TodoItem[]>([])
+
+  const handleCheckTodo = (e) => {
+    console.log(e)
+  }
+
+  React.useEffect(() => {
+    // Sort todos by prio
+    const p1Buffer = [] as TodoItem[]
+    const p2Buffer = [] as TodoItem[]
+    const p3Buffer = [] as TodoItem[]
+    const p4Buffer = [] as TodoItem[]
+
+    data.forEach((todo) => {
+      switch (todo.priority) {
+        case 1:
+          p1Buffer.push(todo)
+
+        case 2:
+          p2Buffer.push(todo)
+
+        case 3:
+          p3Buffer.push(todo)
+
+        case 4:
+          p4Buffer.push(todo)
+
+        default:
+          console.log('Missing or invalid priority: ', todo.priority)
+      }
+    })
+
+    setPrio1Todos(p1Buffer)
+    setPrio2Todos(p2Buffer)
+    setPrio3Todos(p3Buffer)
+    setPrio4Todos(p4Buffer)
+  }, [])
+
+  console.log(prio1Todos)
+  console.log(prio2Todos)
+  console.log(prio3Todos)
+  console.log(prio4Todos)
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      <div className="my-4">
+        <label
+          htmlFor="currentTodo"
+          className="block text-white text-sm font-bold mb-2"
+        >
+          What's on your mind today?
+        </label>
+        <input
+          type="text"
+          id="currentTodo"
+          className="w-full px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:border-blue-500 focus:ring focus:ring-blue-200 text-black"
+          placeholder="I wanna..."
+          value={currentTodo}
+          onChange={(e) => setCurrentTodo(e.target.value)}
         />
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div>
+          <h3>Today (x) [prio: 1]</h3>
+          <ul>
+            {prio1Todos.map((item) => (
+              <li key={item.title}>
+                <label className="flex items-center">
+                  <input
+                    type="checkbox"
+                    className="form-checkbox text-indigo-600 h-5 w-5"
+                    checked={item.completed}
+                    onChange={() => handleCheckTodo(item)}
+                  />
+                  {item.title}
+                </label>
+                <span>❌</span>
+                <select
+                  // value={selectedOption}
+                  // onChange={handleSelectChange}
+                  className="form-select w-full p-2 border rounded-md shadow-sm focus:outline-none focus:border-blue-500 focus:ring focus:ring-blue-200"
+                >
+                  <option value="" disabled>
+                    Select an option
+                  </option>
+                  <option value="option1">Option 1</option>
+                  <option value="option2">Option 2</option>
+                  <option value="option3">Option 3</option>
+                  <option value="option4">Option 4</option>
+                </select>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div>This week (x) [prio: 2]</div>
+        <div>Some day (x) [prio: 3]</div>
+        <div>Idea bank (x) [prio: 4]</div>
       </div>
+
+      <h3>raw data</h3>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </main>
   )
 }
